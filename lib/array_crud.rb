971939768d1e94130dd7a85e31e0896863211879
Sandie Nunez <snunez@vt.edu>
2020-04-01 17:01:36 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,37 @@
-def create_an_empty_array
-  
+require "pry"
+def create_an_empty_array 
+  empty_array = []
 end
 
 def create_an_array
-  
+  ["orange", "apple", "grapes", "blueberries"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element 
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(array,index)
+  array[index]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
-end
+  array[-1]
+end  
+
