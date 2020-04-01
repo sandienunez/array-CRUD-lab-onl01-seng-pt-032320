0aef93f84863ddb30b26e78f25d9fb9a0a8c86d0
Sandie Nunez <snunez@vt.edu>
2020-04-01 16:33:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,6 +10,10 @@ end
 def add_element_to_end_of_array(array, element)
   array << element 
 end
+
+def add_element_to_start_of_array(array, element)
+    array.unshift element
+end
   
 
 def add_element
